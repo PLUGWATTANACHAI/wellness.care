4794d1f62babc72bb40b6100a46a2d9a58cc51d0
Plugpao :)) <plugpao@administrators-MacBook-Pro-28.local>
2026-07-05 23:53:43 +0700

    Force Expo Android package import for autolinking

--- apps/mobile/react-native.config.js
+++ apps/mobile/react-native.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+const path = require("node:path");
+
+module.exports = {
+  dependencies: {
+    expo: {
+      platforms: {
+        android: {
+          sourceDir: path.join(__dirname, "node_modules/expo/android"),
+          packageImportPath: "import expo.modules.ExpoModulesPackage;",
+          packageInstance: "new ExpoModulesPackage()",
+        },
+        ios: {},
+      },
+    },
+  },
+};
